bc9af04f8b5f987c4b7ec1497699f73b1b8009b0
Paul Harrison <pfh@logarithmic.net>
2012-06-27 12:13:27 +1000

    Update workflow to correctly use reference.shrimp_command

--- tail_tools/workflows.py
+++ tail_tools/workflows.py
@@ -76,8 +76,7 @@ class Analyse_polya(config.Action_with_output_dir):
         cores = nesoni.coordinator().get_cores()
         
         nesoni.Execute(
-            command = reference.shrimp_command(cs=True) + 
-                      [ clipped_filename, '-N', str(cores) ],
+            command = reference.shrimp_command(cs=True, parameters=[ clipped_filename, '-N', str(cores) ]),
             output=raw_filename,
             cores=cores,
         ).make()
